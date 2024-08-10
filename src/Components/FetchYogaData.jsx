f458bdd8102543e5af0d927eedac84b14354c6b9
Peter Glover <peter@peterglover.com>
2024-08-09 19:02:11 -0700

    Completed classwork for exercise

--- src/Components/FetchYogaData.jsx
+++ src/Components/FetchYogaData.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import UseFetch from './UseFetch';
 import './FetchData.css';
 
-const FetchData = () => {
-  const [data] = UseFetch('https://api.npoint.io/9045c260b1565daa9e15');
+const FetchYogaData = () => {
+  const [data] = UseFetch('https://api.npoint.io/4459a9a10e43812e1152');
   console.log(data);
   return (
     <>
@@ -13,9 +13,8 @@ const FetchData = () => {
           <>
             <li className='list_data'>
               <h3>{e.name}</h3>
-              <p><strong>Importance: </strong>{e.importance}</p>
-              <p><strong>Benefits: </strong>{e.benefits6}</p>
-              <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>
+              <p><strong>Benefits: </strong>{e.benefits}</p>
+              <p><strong>Time Duration: </strong>{e.time_duration}</p>
             </li>
           </>
         ))}
@@ -24,4 +23,4 @@ const FetchData = () => {
   )
 }
 
-export default FetchData
+export default FetchYogaData
